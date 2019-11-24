5b28c37e92d901078930bcc620f7d5e3150e0cd3
wujiuye <postmaster@wujiuye.com>
2019-11-24 22:23:31 +0800

    更新说明文档

--- src/test/java/com/wujiuye/asyncframework/interfaces/AsyncMessageSubscribe.java
+++ src/test/java/com/wujiuye/asyncframework/interfaces/AsyncMessageSubscribe.java
@@ -1,11 +1,14 @@
 package com.wujiuye.asyncframework.interfaces;
 
+import com.wujiuye.asyncframework.AsyncFunction;
+
 /**
  * @author wujiuye
  * @version 1.0 on 2019/11/24 {描述：}
  */
 public interface AsyncMessageSubscribe {
 
+    @AsyncFunction
     void pullMessage(String queue);
 
 }
